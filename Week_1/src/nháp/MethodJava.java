631d3a57502538e41aae5ab2ff18f1e0eb518feb
Sinh Cute <baosinh2k@gmail.com>
2022-05-18 16:30:28 +0700

    ptbn

--- Week_1/src/nháp/MethodJava.java
+++ Week_1/src/nháp/MethodJava.java
@@ -2,14 +2,6 @@ package nháp;
 
 public class MethodJava {
     public static void main(String[] args) {
-        System.out.println("Sum from 1 to 10 is " + sum(1, 10));
-        System.out.println("Sum from 20 to 37 is " + sum(20, 37));
-        System.out.println("Sum from 35 to 49 is " + sum(35, 49));
+
     }
-        public static int sum ( int a, int b) {
-            int result = 0;
-            for (int i = a; i <= b; i++)
-                result += i;
-            return result;
-        }
 }
