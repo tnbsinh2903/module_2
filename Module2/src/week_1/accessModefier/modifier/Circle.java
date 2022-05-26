042f4570c5d775b970f43cbab47be018a2d99adf
Sinh Cute <baosinh2k@gmail.com>
2022-05-26 17:20:27 +0700

    accessModifier

---
+++ Module2/src/week_1/accessModefier/modifier/Circle.java
@@ -0,0 +1,19 @@
+package week_1.accessModefier.modifier;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+    public Circle(){
+    }
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public double getArea(){
+        return radius * radius * Math.PI;
+    }
+
+}
---
+++ Module2/src/week_1/accessModefier/modifier/TestCircle.java
@@ -0,0 +1,8 @@
+package week_1.accessModefier.modifier;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle(3.5);
+        System.out.println(circle.getArea());
+    }
+}
